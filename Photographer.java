14528a7d30639d67c4ed8a1447753b67ccadd0d3
Dominic <mrdjfraser@gmail.com>
2017-04-11 16:52:05 +0100

    hashmap added

--- Photographer.java
+++ Photographer.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 public class Photographer{
 
   private ArrayList<Camera> cameras;
+  private HashMap<String, Integer> journal;
 
   public Photographer(){
     this.cameras = new ArrayList<Camera>();
+    this.journal = new HashMap<String, Integer>();
   }
 
   public void addCamera(Camera camera){
