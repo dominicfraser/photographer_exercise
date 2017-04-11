0a14b3c14c5ac7135f044f3f04f70c3660227111
Dominic <mrdjfraser@gmail.com>
2017-04-11 15:38:48 +0100

    hasCamera passing

--- Photographer.java
+++ Photographer.java
@@ -1,6 +1,20 @@
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Photographer{
 
-  
+  private ArrayList<Camera> cameras;
+
+  public Photographer(){
+    this.cameras = new ArrayList<Camera>();
+  }
+
+  public void addCamera(Camera camera){
+    cameras.add(camera);
+  }
+
+  public int cameraCount(){
+    return cameras.size();
+  }
+
 }
--- PhotographerTest.java
+++ PhotographerTest.java
@@ -4,16 +4,20 @@ import org.junit.*;
 public class PhotographerTest{
 
   Photographer photographer;
+  DigitalCamera digitalCamera;
 
   @Before
   public void before(){
-    photographer = new Photographer;
+    photographer = new Photographer();
+    digitalCamera = new DigitalCamera();
+  }
+
+  @Test 
+  public void hasCamera(){
+    photographer.addCamera(digitalCamera);
+    assertEquals( 1,photographer.cameraCount() );
   }
 
 
-
-
-
-
-
 }
+
